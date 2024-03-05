01a13f2f168f0b3656ca184028483e846252243d
Phạm Long <longpd3.avitex@gmail.com>
2024-03-05 18:28:05 +0700

    first commit

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,6 @@
 import { createStore, applyMiddleware } from "redux";
 import { persistStore, persistReducer } from "redux-persist";
-import { composeWithDevTools } from "redux-devtools-extension";
+// import { composeWithDevTools } from "redux-devtools-extension";
 import storage from "redux-persist/lib/storage";
 
 import autoMergeLevel2 from "redux-persist/lib/stateReconciler/autoMergeLevel2";
@@ -15,7 +15,7 @@ const persistConfig = {
 const pReducer = persistReducer(persistConfig, rootReducer);
 const createdStore = createStore(
   pReducer,
-  composeWithDevTools(applyMiddleware())
+  // composeWithDevTools(applyMiddleware())
 );
 
 export const initializeStore = (initialState = {}) => {
