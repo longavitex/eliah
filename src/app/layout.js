13a0b7f2c5fc50ae955ac8f207a4611da113bf4f
Phạm Long <longpd3.avitex@gmail.com>
2024-03-06 17:19:56 +0700

    first commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,7 +13,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
+      <body className={spartan.className}>
         <GlobalProvider>
           {children}
           <div id="nav-full" />
